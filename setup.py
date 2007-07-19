136e7dd91b59cf3ec32563747143c1d027ecdb0d
Fred Drake <fdrake@acm.org>
2007-07-19 15:18:19 +0000

    bump version; setup.py should always contain the next stable release number

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ open('documentation.txt', 'w').write(long_description)
 
 setup(
     name='zope.testing',
-    version='3.5.0',
+    version='3.5.1',
     url='http://www.python.org/pypi/zope.testing',
     license='ZPL 2.1',
     description='Zope testing framework, including the testrunner script.',
