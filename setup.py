b040c77da7ce00c786af63529d34df7e839ec712
Tres Seaver <tseaver@palladion.com>
2008-09-22 13:33:53 +0000

    Prep 3.7.0 release.

--- setup.py
+++ setup.py
@@ -79,8 +79,8 @@ open('documentation.txt', 'w').write(long_description)
 
 setup(
     name='zope.testing',
-    version='3.7dev',
-    url='http://www.python.org/pypi/zope.testing',
+    version='3.7.0',
+    url='http://pypi.python.org/pypi/zope.testing',
     license='ZPL 2.1',
     description='Zope testing framework, including the testrunner script.',
     long_description=long_description,
