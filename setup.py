98555cccae585b86752cbae1fed468be8abc914b
Benji York <benji@benjiyork.com>
2009-04-13 19:27:43 +0000

    remove "dev" from version

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ open('documentation.txt', 'w').write(long_description)
 
 setup(
     name='zope.testing',
-    version='3.7.2dev',
+    version='3.7.2',
     url='http://pypi.python.org/pypi/zope.testing',
     license='ZPL 2.1',
     description='Zope testing framework, including the testrunner script.',
