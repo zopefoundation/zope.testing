188baa1be3f28e218328e6757151db96c7c3ce66
Marius Gedminas <marius@gedmin.as>
2010-01-26 12:47:55 +0000

    Back to development: 3.8.8

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ open('documentation.txt', 'w').write(long_description)
 
 setup(
     name='zope.testing',
-    version = '3.8.7',
+    version = '3.8.8dev',
     url='http://pypi.python.org/pypi/zope.testing',
     license='ZPL 2.1',
     description='Zope testing framework, including the testrunner script.',
