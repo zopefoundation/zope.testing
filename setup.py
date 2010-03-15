87415917d10121d5de49458632e8b1784d2e5864
Godefroid Chapelle <gotcha@bubblenet.be>
2010-03-15 09:39:46 +0000

    Back to development: 3.9.3

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ open('documentation.txt', 'w').write(long_description)
 
 setup(
     name='zope.testing',
-    version = '3.9.2',
+    version = '3.9.3dev',
     url='http://pypi.python.org/pypi/zope.testing',
     license='ZPL 2.1',
     description='Zope testing framework, including the testrunner script.',
