ac8fc36b99f69524c73c17256f479c698dc081ed
Marius Gedminas <marius@gedmin.as>
2010-03-26 17:09:45 +0000

    Preparing release 3.9.3

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ open('documentation.txt', 'w').write(long_description)
 
 setup(
     name='zope.testing',
-    version = '3.9.3dev',
+    version = '3.9.3',
     url='http://pypi.python.org/pypi/zope.testing',
     license='ZPL 2.1',
     description='Zope testing framework, including the testrunner script.',
