f0afb6c216276a74eaa832eac52ac6ec090245df
Tres Seaver <tseaver@palladion.com>
2011-11-02 15:28:37 +0000

    svb

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ long_description=(
 
 setup(
     name='zope.testing',
-    version='3.9.7',
+    version='3.9.8dev',
     url='http://pypi.python.org/pypi/zope.testing',
     license='ZPL 2.1',
     description='Zope testing framework, including the testrunner script.',
