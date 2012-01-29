11f6075d58bec126b5724de231facb159165b93d
Jim Fulton <jim@zope.com>
2012-01-29 14:23:43 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ long_description=(
 
 setup(
     name='zope.testing',
-    version = '4.0.1dev',
+    version = '4.1.0',
     url='http://pypi.python.org/pypi/zope.testing',
     license='ZPL 2.1',
     description='Zope testing helpers',
