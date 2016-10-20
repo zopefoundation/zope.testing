e71e6a8bf9c5551b331d3bd5d890e06555f4bc58
Michael Howitz <mh@gocept.com>
2016-10-20 07:50:57 +0200

    Back to development: 4.7.0

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ keywords = "zope testing doctest RENormalizing OutputChecker timeout logging"
 
 setup(
     name='zope.testing',
-    version='4.6.0',
+    version='4.7.0.dev0',
     url='https://github.com/zopefoundation/zope.testing',
     license='ZPL 2.1',
     description='Zope testing helpers',
