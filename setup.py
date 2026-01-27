e5efd0398f261593ea731dc1ee0d1e5385dc27c5
Michael Howitz <icemac@gmx.net>
2026-01-27 09:14:47 +0100

    Move package metadata from setup.py to pyproject.toml. (#60)

--- setup.py
+++ setup.py
@@ -18,62 +18,9 @@
 ##############################################################################
 """Setup for zope.testing package
 """
-import os
 
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-long_description = read('README.rst') + '\n\n' + read('CHANGES.rst')
-keywords = "zope testing doctest RENormalizing OutputChecker timeout logging"
-
-setup(
-    name='zope.testing',
-    version='6.2.dev0',
-    url='https://github.com/zopefoundation/zope.testing',
-    license='ZPL-2.1',
-    description='Zope testing helpers',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Zope Public License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Framework :: Zope :: 3",
-        "Framework :: Zope :: 4",
-        "Framework :: Zope :: 5",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Software Development :: Testing",
-    ],
-    keywords=keywords,
-    python_requires='>=3.10',
-    extras_require={
-        'test': [
-            'zope.testrunner >= 6.4',
-        ],
-        'docs': [
-            'sphinx',
-            'repoze.sphinx.autointerface',
-            'zope.exceptions',
-            'zope.interface',
-        ],
-    },
-    include_package_data=True,
-    zip_safe=False,
-)
+# See pyproject.toml for package metadata
+setup()
